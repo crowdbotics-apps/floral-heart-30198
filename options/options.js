dcd9142989256b13b64d54ad0224258fc5f944b0
Crowdbotics <team@crowdbotics.com>
2021-08-30 15:17:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "floral_heart_30198Identifier",
+  url: "https://floral_heart_30198Identifier.botics.co",
+  api: "https://floral_heart_30198Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
